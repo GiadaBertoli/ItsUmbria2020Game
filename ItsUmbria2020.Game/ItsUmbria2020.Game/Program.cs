d682c590de04fb554d83cb28d0668cbd758fbf20
Giada Bertoli <allievo_bertoli.g@itsumbria.it>
2020-09-30 12:53:16 +0200

    commit

--- ItsUmbria2020.Game/ItsUmbria2020.Game/Program.cs
+++ ItsUmbria2020.Game/ItsUmbria2020.Game/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ItsUmbria2020.OnlineGame.Library.Models.Characters;
+using System;
+using System.Reflection.PortableExecutable;
 
 namespace ItsUmbria2020.Game
 {
@@ -6,7 +8,15 @@ namespace ItsUmbria2020.Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Character character = new Wizard();
+            PrintCharacters(new Wizard());
+        }
+
+        private static void PrintCharacters(Character character)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(character.Name);
+            Console.ResetColor();
         }
     }
 }
---
+++ ItsUmbria2020.Game/ItsUmbria2020.OnlineGame.Library.Models/Characters/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItsUmbria2020.OnlineGame.Library.Models.Characters
+{
+    public abstract class Character
+    {
+        public string Name { get; set; } = "Player";
+        public Gender Gender { get; set; }
+        public CharacterType Type { get; set; }
+    }
+    public class Wizard : Character
+    {
+
+    }
+    public enum Gender
+    {
+        Male,
+        Female,
+        Unknown
+    }
+    public enum CharacterType
+    {
+        Human,
+        Eif,
+        Orc
+    }
+
+}
