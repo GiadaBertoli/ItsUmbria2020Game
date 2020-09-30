abe48d3f4b63436d04170e84c61f2b0a32cfb634
GiadaBertoli <72126452+GiadaBertoli@users.noreply.github.com>
2020-09-30 12:12:51 +0200

    Add files via upload

---
+++ ItsUmbria2020.Game/ItsUmbria2020.Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItsUmbria2020.Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
